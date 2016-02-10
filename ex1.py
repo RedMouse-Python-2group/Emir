0996cbab67a86cbbb406de300763c04535253608
metr1ckzu <metr1ckzu@gmail.com>
2016-02-10 20:49:47 +0600

    first lesson's excersises

---
+++ ex1.py
@@ -0,0 +1,13 @@
+x = int(raw_input('Input a number in range 1-9: '))
+if x >= 1 and x <= 3:
+    s = str(raw_input('Please, give a string: '))
+    n = int(raw_input('Please, give a multiplier: '))
+    for n in s:
+        print s
+if x >= 4 and x <= 6:
+    m = int(raw_input('Please, give a power of number: '))
+    print(x ** m)
+if x >= 7 and x <= 9:
+    for k in range(x, x+10):
+        k += 1
+        print k
