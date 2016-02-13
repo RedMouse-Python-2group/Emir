2288e134d17cfe47cba3c21059533a57f558c28a
metr1ckzu <metr1ckzu@gmail.com>
2016-02-13 19:50:26 +0600

    edited lesson2.ex1.py

--- lesson2-ex1.py
+++ lesson2-ex1.py
@@ -1,10 +1,12 @@
 def lesson2():
     program = raw_input("""Please select a program:
-    1) max word with space splitter
-    2) max word with semicolon splitter
-    3) min word with user specified symbol
 
-    Type number of program: \n""")
+1) max word with space splitter
+2) max word with semicolon splitter
+3) min word with user specified symbol
+4) count words in sentence
+
+Type number of program: \n""")
 
     if program == '1':
         space_max()
@@ -12,6 +14,8 @@ def lesson2():
         semicolon_max()
     elif program == '3':
         min_specified()
+    elif program == '4':
+        sent_count()
     elif program == 'exit':
         pass
     else:
@@ -33,5 +37,10 @@ def min_specified():
     text = str(raw_input('Input your text: \n'))
     x = text.split(symbol)
     print min(x, key=len)
+
+def sent_count():
+    text = str(raw_input('Input your sentence: \n'))
+    x = text.split(' ')
+    print len(x)
 
 lesson2()
